5d23658825f92255d36ea9bd6ea991e7943ad1fa
luitor2014 <luisxxd_16@hotmail.com>
2015-11-27 03:15:50 -0500

    Update tcp_server.js

--- tcp_server.js
+++ tcp_server.js
@@ -1,9 +1,3 @@
-var readline = require('readline');
-var rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-  terminal: false
-});
 
 var PORT = process.env.PORT;
 var net = require('net');
@@ -35,9 +29,6 @@ var server = net.createServer(function(socket) {
     // Log it to the server output too
     process.stdout.write(message)
   }
-  rl.on('line', function(dt){
-    socket.write(dt);
-  });
 });
 
 server.listen(PORT,'127.0.0.1', function() {
@@ -46,25 +37,3 @@ server.listen(PORT,'127.0.0.1', function() {
 setInterval(function(){
   console.log("Escuchando...");
 },4000);
-var os = require('os');
-var ifaces = os.networkInterfaces();
-
-Object.keys(ifaces).forEach(function (ifname) {
-  var alias = 0;
-
-  ifaces[ifname].forEach(function (iface) {
-    if ('IPv4' !== iface.family || iface.internal !== false) {
-      // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
-      return;
-    }
-
-    if (alias >= 1) {
-      // this single interface has multiple ipv4 addresses
-      console.log(ifname + ':' + alias, iface.address);
-    } else {
-      // this interface has only one ipv4 adress
-      console.log(ifname, iface.address);
-    }
-    ++alias;
-  });
-});
