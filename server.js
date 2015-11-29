f9f932ff3e20abca9d168156f22615a5c01773a9
luitor2014 <luisxxd_16@hotmail.com>
2015-11-29 00:54:12 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var net = require('net');
 //var sys = require('sys');
 var count = 0;
 var port = process.env.PORT || 1337;
-var host = 'app44364213@heroku.com'; // heroku-app-name when deployed
+var host = '54.235.132.133'; // heroku-app-name when deployed
 var server_host = "tcpserver123.herokuapp.com"; 
 var server = net.createServer(function (socket) { 
 
